6288344381afe14757c8513fda585dd32ba81188
mdteixeira <matheusdiegoteixeira@gmail.com>
2023-05-02 10:47:33 -0300

    aa

---
+++ JS/AULAS JS5/aula01.js
@@ -0,0 +1,12 @@
+/** 
+ * Para comentar várias linhas, utilizamos o "/*"
+ * 
+*/
+var nome= "Bruno"; // estou declarando uma variavel "nome"
+// var nome= "Bruno;"
+var nota = 10;
+var produto = "Refrigerante";
+var valor = 6.00;
+
+let VouTrabalhar = false
+console.log ("Ola Mundo");
