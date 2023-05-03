704e8b46c86fa96e4bec77fd7ff35544c3d90330
mdteixeira <matheusdiegoteixeira@gmail.com>
2023-05-03 09:34:12 -0300

    aa

--- JS/AULAS JS5/aula02pt2.js
+++ JS/AULAS JS5/aula02pt2.js
@@ -1,2 +1,7 @@
 var media = (7 + 2) / 2; 
-console.log (media);                                          
+console.log (media);    
+// > | >=
+// < | <=
+// == | ===
+console.log (2 =="2")                                    
+console.log(2 === 2)
