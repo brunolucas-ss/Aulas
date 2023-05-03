d10492bc92b142703151686867f7c3e14ae269a2
mdteixeira <matheusdiegoteixeira@gmail.com>
2023-05-03 09:34:18 -0300

    aa

--- JS/AULAS JS5/aula02.js
+++ JS/AULAS JS5/aula02.js
@@ -14,7 +14,7 @@ var num3 = Infinity;
 var sala ;
 console.log ( typeof num3);
 console.log ( typeof sala);
-console.log ( mult + num1 + mult);
+console.log ( mult + num1 + multaa);
 console.log ( 'Olá, bom dia \'');
 console.log ('Olá, Bom dia! \b Aluno Bruno');
 
