9f1c1e1c79153b544afe1c11b913b70fcda0ada8
brunolucas-ss <b.lucas.ssilva@gmail.com>
2023-05-04 10:34:44 -0300

    aa

--- JS/AULAS JS5/aula03.js
+++ JS/AULAS JS5/aula03.js
@@ -1,3 +1,16 @@
- var media= 5;
- media*= 9;
-console.log(media)
+//  var media= 5;
+//  media*= 9;
+// console.log(media)
+// let x = 10;
+// let y = 8;
+// console.log()
+var valor = 549.99
+const  marca = "Nike"
+let tipo = "tenis"
+console.log( ` a ${marca} Tem  um ${tipo}  muito confortavel que custa R$${valor}   `)
+{
+let tipo2 = "boné "
+var valor2   = "159.00"
+console.log( ` a ${marca} Tem  um ${tipo2}  muito bonito que custa R$${valor2}   `)
+}
+// let idade= 18 // Não pode ser redeclarado
---
+++ JS/AULAS JS5/aula03pt2.js
@@ -0,0 +1,13 @@
+window.alert("Olá,Mundo!");
+let nome = window.prompt("Qual é o seu nome?")
+alert (`Olá ${nome},Tudo bem? Vamos fazer a soma`);
+
+let num1 = Number(prompt (' "Digite um número:" '));
+let num2 = Number(prompt(' "Digite um número:" '));
+let res = num1 + num2;
+
+// let res = Number(num1) + Number(num2);
+
+
+alert (`A soma dos valores ${num1} e ${num2} é: ${res}`);
+console.warn("Cuidado, Você é calvo")
