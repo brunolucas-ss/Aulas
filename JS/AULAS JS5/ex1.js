00a5ef90691bbac018849c4539483f653976eb11
brunolucas-ss <b.lucas.ssilva@gmail.com>
2023-05-08 11:59:03 -0300

    aa

---
+++ JS/AULAS JS5/ex1.js
@@ -0,0 +1,23 @@
+var n1 = 8;
+var n2 = 8;
+
+let media = (n1 + n2) /2;
+console.log(`Média ${media}`)
+if (media >=7) {
+    console.log(`Aprovado`);
+}
+else{
+    console.log(`Reprovado`);
+}
+console.log(`Fim do programa`);
+// ======================================   
+let tenhoHabilitação = true ; 
+if (tenhoHabilitação == true) {
+    console.log(`Pode dirigir`);
+} 
+else if(tenhoHabilitação == false) {
+    console.log(`Não Pode dirigir`);
+}
+// =======================================
+var PI= 3.1415; 
+console.log(`O valor de PI é ${PI} `)
---
+++ JS/AULAS JS5/if.js
@@ -0,0 +1,21 @@
+let tenhoHabilitação = false ; 
+if (tenhoHabilitação == true) {
+    console.log(`Pode dirigir`);
+} 
+else if(tenhoHabilitação == false) {
+    console.log(`Não Pode dirigir`);
+}
+
+    
+
+
+
+let media = (n1 + n2) /2;
+console.log(`Média ${media}`)
+if (media >=7) {
+    console.log(`Aprovado`);
+}
+else{
+    console.log(`Reprovado`);
+}
+console.log(`Fim do programa`);
