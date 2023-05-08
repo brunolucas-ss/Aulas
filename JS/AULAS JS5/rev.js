19c6ac9eb597594e23f37cc29923f680fcaf55d8
brunolucas-ss <b.lucas.ssilva@gmail.com>
2023-05-08 09:27:00 -0300

    aa

---
+++ JS/AULAS JS5/rev.js
@@ -0,0 +1,20 @@
+// window.alert('Olá')
+// let NomeUsuario = prompt(`Qual é o seu nome?`)
+// alert (`Olá ${NomeUsuario}! Tudo bem com você?`)
+// let num1 = Number(prompt(`Digite um número:`))
+// let num2 = Number(prompt(`Digite outro número:`))
+// let soma = num1 + num2
+// alert (`a soma de ${num1} + ${num2} é igual a ${soma}`)
+//  const str1 = 'A raposa é um animal esperto'
+//  const palavras = str1.split(' ');
+// console.log(palavras[3]);
+// Saída esperada: "um"
+
+// trim()-Correção de espaços
+//search
+var ola = "Olá, alunos!"
+console.log(ola)
+var ola = "Olá, alunos!".replace("Alunos!","Bia!");
+console.log(ola)
+var ola = "Olá, alunos!".replace("Alunos!","Bia!");
+console.log(ola.replace("Alunos!", "Bia"));
