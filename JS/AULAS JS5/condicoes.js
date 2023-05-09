443e3541e9f000a9dbd41423c209f9121f5c4155
brunolucas-ss <b.lucas.ssilva@gmail.com>
2023-05-09 10:20:19 -0300

    aa

---
+++ JS/AULAS JS5/condicoes.js
@@ -0,0 +1,43 @@
+// let testeValor = true;
+// let media = 7 >= 9.
+// console.log(testeValor)
+// console.log(media)
+
+// let salario = 1320;
+// let irPraia = salario >= 1500 ? irpraia :" Vai Ficar em casa"
+// console.log(irPraia)
+let pontosAluno =""
+
+switch (pontosAluno) {
+    case "ptsBonus1":
+     console.log("2.0pts");
+    break;
+
+    case "ptsBonus2":
+    console.log("1.5pts");
+    break; 
+
+    case "ptsBonus3":
+    console.log("1.0pts");
+    break;
+
+    case "ptsBonus4":
+    console.log("0.5pts");
+    break;
+
+    case "ptsBonus5":
+    console.log("0.25pts");
+    break;
+
+    default: 
+    console.log("Não foi aplicado pontos adicionais");
+    break
+}
+console.log("Fim do programa")
+
+let idade = 14;
+while (idade < 18) {
+    console.log(`Não pode beber ${idade}`);
+    idade +=1;
+}
+console.log("Fim do programa")
---
+++ JS/AULAS JS5/ex.2.js
@@ -0,0 +1,11 @@
+window.alert("bom dia")
+let nome = prompt('qual seu nome?');
+let idade = prompt('qual sua idade?');
+let bairro = prompt('onde você mora?') ;
+let num1 = Number(prompt(`Digite um número:`))
+let num2 = Number(prompt(`Digite outro número:`))
+let soma = num1 + num2
+alert (`a soma de ${num1} + ${num2} é igual a ${soma}`)
+let res = number + num2 ;
+window.alert(res);
+window.alert(nome.toLocaleUpperCase()); 
