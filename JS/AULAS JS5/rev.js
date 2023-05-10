252e12b05485ec72165c85a1c3d3c53e635f6035
brunolucas-ss <b.lucas.ssilva@gmail.com>
2023-05-10 10:07:09 -0300

    aa

--- JS/AULAS JS5/rev.js
+++ JS/AULAS JS5/rev.js
@@ -23,5 +23,42 @@
 // var ola = "Olá, alunos!".replace("alunos!","Bia!");
 // console.log(ola.replace("alunos!", "Bia"));
 
-let nome = ["Bruno" ,"Lucas" ,"Sapo" ,5 ,9 ,true ,,false ]
-console.log(nome);
+// let nome = ["Bruno","Lucas","Sapo",5 ,9,true,,false]
+// console.log(nome);
+// console.log(nome[4 , 7]);
+
+// let mercado = [
+//     ["Arroz", "Feijão", "Açucar"],
+//     ["Veja", "Detergente", "Candida"],
+//     ["Chocolate", "Danone", "Biscoito"],
+// ];
+// console.log(mercado[2][0])
+
+// mercado [2][2] = "Fini"
+// console.log(mercado [2][2]);
+
+// let numeros = [1, 2, 3, -1, -2]
+// console.log(numeros.join(' - '))
+// console.log(numeros);
+// console.log(typeof numeros);
+
+// let numeros2 = numeros.toString();
+// console.log(numeros2);
+// console.log(typeof numeros2);
+let frutas = ["banana","Morango","Mamão","Kiwi","Melão"];
+for (let frutas2 = 0; frutas2 < frutas.length; frutas2 +=1) {
+    console.log (`eu gosto de: ${frutas[frutas2]}`);
+}
+
+// //Arrays unidimensionais
+// // Podemos criar array assim
+// const num1 = new Array(30, -1, 5, 3, 121);
+// // Ou podemos fazer
+// const num2 = [-2, 40, 16, 111, 33, 64];
+// console.log(num1);
+// console.log(num2);
+// // Acessando elementos diversos dos arrays
+// console.log(`primeiro elemento de num1: ${num1[0]}`);
+// console.log(`primeiro elemento de num2: ${num2[0]}`);
+// console.log(`terceiro elemento de num1: ${num1[2]}`);
+// console.log(`quinto elemento de num2: ${num2[5]}`);
