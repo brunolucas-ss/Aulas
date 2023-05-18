2bb1fde7514435d83f7848d88fe3ebdc587cc137
brunolucas-ss <b.lucas.ssilva@gmail.com>
2023-05-18 08:24:25 -0300

    aa

---
+++ JS/AULAS JS5/funçoes3.js
@@ -0,0 +1,19 @@
+function media (n1=0, n2=0){
+    return(n1+n2)/2;
+}
+console.log(media(8));
+const soma2= (num1 = 0, num2= 0)=>{
+    return num1+num2
+}
+console.log(soma2(8,5))
+
+
+//imc 
+
+let altura = 156
+let peso = 60
+var imc = peso / (altura * altura);
+function IMC(peso, altura) {
+    return peso / (altura * altura);
+  }
+  console.log(imc)
---
+++ JS/AULAS JS5/node_a28a42bb0fde7.tmp.js
@@ -0,0 +1,7 @@
+let altura = 156
+let peso = 60
+var imcCal = peso/ (altura * altura);
+function IMC(peso, altura) {
+    return peso / (altura * altura);
+  }
+  console.log(imcCal)
