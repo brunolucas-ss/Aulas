3a5aa3c779279d0021e1c72acfb2a90fb4e2fb4e
brunolucas-ss <b.lucas.ssilva@gmail.com>
2023-05-18 09:06:24 -0300

    aa

---
+++ JS/AULAS JS5/revd18.js
@@ -0,0 +1,26 @@
+// for (let i = 0; i <= 10; i++) {
+//     console.log(`Contador:${i}`)
+// }
+// console.log("---------------------");
+let salaAula = ["Vitor", "Samuel", "Bruno","Hilary","Day"];
+// console.log(salaAula[0]);
+// console.log(salaAula[1]);
+// console.log(salaAula[2]);
+// console.log(salaAula[3]);
+// console.log(salaAula[4]);
+// console.log("---------------------");
+
+
+
+const nomes =(nome, ind) => {
+    return console.log(`Olá aluno: ${nome} está na chamada num:${ind}`)
+}
+salaAula.forEach((nome,ind) =>{
+    console.log(`Os Alunos aprovados são:${nome} está na chamada num:${ind} `)
+});
+console.log("-------------------------------------------"); 
+
+// let cores=["vermelho","azul","verde", "amarelo"]
+// cores.forEach((item,num,Array) => ${nomes}
+// )
+
