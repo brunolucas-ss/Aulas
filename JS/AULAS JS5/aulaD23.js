ed21433bdc2128f87a9f8fa25e5cc5736e5ae220
brunolucas-ss <b.lucas.ssilva@gmail.com>
2023-05-23 09:52:01 -0300

    aa

--- JS/AULAS JS5/aulaD23.js
+++ JS/AULAS JS5/aulaD23.js
@@ -1,8 +1,20 @@
-const media = (n1,n2) => {
-    return res= (n1+n2) / 2;
-}
-function nota (n1,n2) {
-    return res = (n1+n2) / 2;
-}
+document.title = "AulaD23";
 
-console.log(window.URL)
+// h1
+let titulo = document.createElement ('h1');
+titulo.innerHTML = "Aula D23";
+document.body.appendChild(titulo);
+titulo.style.textAlign = 'center'
+titulo.style.color = 'blueviolet'
+titulo.style.backgroundColor = 'black'
+
+// h2
+let subtitulo = document.createElement ('h2');
+subtitulo.innerHTML = "Aula DOM";
+document.body.appendChild(subtitulo);
+
+let texto = document.createElement ('p');
+texto.innerHTML = "Meu texto Meu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu textoMeu texto ";
+document.body.appendChild(texto);
+texto.style.backgroundColor = 'blueviolet'
+texto.style.color = 'white'
